9b8db594849a091f131c4efb92b02577df85c0a3
Zsolt Prontvai <prozsolt@gmail.com>
2020-01-30 13:59:21 +0000

    Add pair detection

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,15 +1,29 @@
 module CardProblem
   class Hand
     def initialize(cards)
-      @cards = cards
+      @cards = cards.map { |c| { suit: c[1], value: c[0] } }
     end
 
     def rank
-      0
+      if pair?
+        1
+      else
+        0
+      end
     end
 
     def <=>(other)
       rank <=> other.rank
+    end
+
+    private
+
+    def pair?
+      @cards.group_by { |c| c[:value] }.values.any? { |e| e.size == 2 }
+    end
+
+    def two_pair?
+      false
     end
   end
 end
