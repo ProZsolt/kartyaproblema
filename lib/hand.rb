490f229a055de30527992ac9c607c90714666526
Zsolt Prontvai <prozsolt@gmail.com>
2020-01-30 18:44:08 +0000

    Finished rank values

--- lib/hand.rb
+++ lib/hand.rb
@@ -24,23 +24,23 @@ module CardProblem
 
     def value
       if straight_flush?
-        [8]
+        value_of_straight_flush
       elsif four_of_a_kind?
-        [7]
+        value_of_four_of_a_kind
       elsif full_house?
-        [6]
+        value_of_full_house
       elsif flush?
-        [5]
+        value_of_flush
       elsif straight?
-        [4]
+        value_of_straight
       elsif three_of_a_kind?
-        [3]
+        value_of_three_of_a_kind
       elsif two_pair?
-        [2]
+        value_of_two_pair
       elsif pair?
-        [1]
+        value_of_pair
       else
-        [0] + @cards.map(&:value).sort.reverse
+        value_of_high_card
       end
     end
 
@@ -50,36 +50,79 @@ module CardProblem
 
     private
 
+    def value_of_high_card
+      [0] + @cards.map(&:value).sort.reverse
+    end
+
     def pair?
       @cards.group_by(&:value).values.any? { |e| e.size == 2 }
+    end
+
+    def value_of_pair
+      pair_value = @cards.group_by(&:value).values.filter { |e| e.size == 2 }.first.first.value
+      remaining_card_values = @cards.filter { |c| c.value != pair_value }.map(&:value).sort.reverse
+      [1] + [pair_value] + remaining_card_values
     end
 
     def two_pair?
       @cards.group_by(&:value).values.filter { |e| e.size == 2 }.size == 2
     end
 
+    def value_of_two_pair
+      pair_values = @cards.group_by(&:value).values.filter { |e| e.size == 2 }.map { |e| e.first.value }.sort.reverse
+      remaining_card_values = @cards.reject { |c| pair_values.include?(c.value) }.map(&:value).sort.reverse
+      [2] + pair_values + remaining_card_values
+    end
+
     def three_of_a_kind?
       @cards.group_by(&:value).values.any? { |e| e.size == 3 }
+    end
+
+    def value_of_three_of_a_kind
+      triplet_value = @cards.group_by(&:value).values.filter { |e| e.size == 3 }.first.first.value
+      [3, triplet_value]
     end
 
     def straight?
       @cards.map(&:value).sort.each_cons(2).all? { |a, b| b == a + 1 }
     end
 
+    def value_of_straight
+      [4, @cards.map(&:value).max]
+    end
+
     def flush?
       @cards.group_by(&:suit).size == 1
+    end
+
+    def value_of_flush
+      [5] + @cards.map(&:value).sort.reverse
     end
 
     def full_house?
       pair? && three_of_a_kind?
     end
 
+    def value_of_full_house
+      triplet_value = @cards.group_by(&:value).values.filter { |e| e.size == 3 }.first.first.value
+      [6, triplet_value]
+    end
+
     def four_of_a_kind?
       @cards.group_by(&:value).values.any? { |e| e.size == 4 }
     end
 
+    def value_of_four_of_a_kind
+      triplet_value = @cards.group_by(&:value).values.filter { |e| e.size == 4 }.first.first.value
+      [7, triplet_value]
+    end
+
     def straight_flush?
       straight? && flush?
+    end
+
+    def value_of_straight_flush
+      [8, @cards.map(&:value).max]
     end
   end
 end
