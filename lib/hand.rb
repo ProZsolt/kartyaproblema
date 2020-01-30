425270765f11705c90e128eadc3357e7246df146
Zsolt Prontvai <prozsolt@gmail.com>
2020-01-30 15:12:20 +0000

    Dumb rank compare

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,11 +1,23 @@
 module CardProblem
   class Hand
     def initialize(cards)
-      @cards = cards.map { |c| { suit: c[1], value: c[0] } }
+      @cards = cards.map { |c| cardstring_to_hash(c) }
     end
 
     def rank
-      if two_pair?
+      if straight_flush?
+        8
+      elsif four_of_a_kind?
+        7
+      elsif full_house?
+        6
+      elsif flush?
+        5
+      elsif straight?
+        4
+      elsif three_of_a_kind?
+        3
+      elsif two_pair?
         2
       elsif pair?
         1
@@ -25,7 +37,48 @@ module CardProblem
     end
 
     def two_pair?
-      @cards.group_by { |c| c[:value] }.values.filter { |e| e.size == 2 }.count == 2
+      @cards.group_by { |c| c[:value] }.values.filter { |e| e.size == 2 }.size == 2
+    end
+
+    def three_of_a_kind?
+      @cards.group_by { |c| c[:value] }.values.any? { |e| e.size == 3 }
+    end
+
+    def straight?
+      @cards.map { |c| c[:value] }.sort.each_cons(2).all? { |a, b| b == a + 1 }
+    end
+
+    def flush?
+      @cards.group_by { |c| c[:suit] }.size == 1
+    end
+
+    def full_house?
+      pair? && three_of_a_kind?
+    end
+
+    def four_of_a_kind?
+      @cards.group_by { |c| c[:value] }.values.any? { |e| e.size == 4 }
+    end
+
+    def straight_flush?
+      straight? && flush?
+    end
+
+    def cardstring_to_hash(str)
+      case str[0]
+      when 'T'
+        { suit: str[1], value: 10 }
+      when 'J'
+        { suit: str[1], value: 11 }
+      when 'Q'
+        { suit: str[1], value: 12 }
+      when 'K'
+        { suit: str[1], value: 13 }
+      when 'A'
+        { suit: str[1], value: 14 }
+      else
+        { suit: str[1], value: str[0].to_i }
+      end
     end
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -14,5 +14,38 @@ describe CardProblem::Hand do
     it 'returns 2' do
       expect(CardProblem::Hand.new(%w[2C 2H 5C 5C 9C]).rank).to eq 2
     end
+
+    it 'returns 3' do
+      expect(CardProblem::Hand.new(%w[2C 2H 2D 5C 9C]).rank).to eq 3
+    end
+
+    it 'returns 4' do
+      expect(CardProblem::Hand.new(%w[7C 8H 9D TC JC]).rank).to eq 4
+    end
+
+    it 'returns 5' do
+      expect(CardProblem::Hand.new(%w[6C 8C KC TC JC]).rank).to eq 5
+    end
+
+    it 'returns 6' do
+      expect(CardProblem::Hand.new(%w[8C 8H 8D JC JH]).rank).to eq 6
+    end
+
+    it 'returns 7' do
+      expect(CardProblem::Hand.new(%w[8C 8H 8D 8S JH]).rank).to eq 7
+    end
+
+    it 'returns 8' do
+      expect(CardProblem::Hand.new(%w[7H 8H 9H TH JH]).rank).to eq 8
+    end
+  end
+
+  describe '#<=>' do
+    it 'returns true' do
+      three_of_a_kind = CardProblem::Hand.new(%w[2C 2H 2D 5C 9C])
+      flush = CardProblem::Hand.new(%w[6C 8C KC TC JC])
+
+      expect(three_of_a_kind <=> flush).to eq(-1)
+    end
   end
 end
